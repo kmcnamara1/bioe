a8d2ee1dca954c27bed16cc059f6908c3286984c
Anna Scolaro <annascolaro98@gmail.com>
2021-04-27 22:36:29 +1000

    Can Enter name and press return and will go to over view screen

--- UI/UI_1and2.py
+++ UI/UI_1and2.py
@@ -2,6 +2,13 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QApplication , QMainWindow , QPushButton , QWidget
 import sys
 
+def loadClinicianName():
+        text_file = open("ClinicanName.txt", "r")
+        data = text_file.read()
+        text_file.close()
+        print(data)
+        return data
+
 class UIWindow(QWidget):
     def __init__(self, parent=None):
         super(UIWindow, self).__init__(parent)
@@ -203,15 +210,17 @@ class UIWindow(QWidget):
         self.label_9.setText(_translate("OverViewWindow", "-/3"))
         self.label_10.setText(_translate("OverViewWindow", "Paitent: Jone Swith"))
         self.label_13.setText(_translate("OverViewWindow", "Clinician"))
-        self.label_14.setText(_translate("OverViewWindow", "Pippa Pepo"))
+
+        clinicianName = loadClinicianName()
+
+        self.label_14.setText(_translate("OverViewWindow", clinicianName ))
+
 
 
 class UIToolTab(QWidget):
     def __init__(self, parent=None):
         super(UIToolTab, self).__init__(parent)
-        self.CPSBTN = QPushButton("text2", self)
-        self.CPSBTN.move(100, 350)
- 
+
         self.centralwidget = QtWidgets.QWidget(self)
         self.centralwidget.setObjectName("centralwidget") 
         self.SideColum_2 = QtWidgets.QColumnView(self.centralwidget)
@@ -261,10 +270,10 @@ class UIToolTab(QWidget):
                                         "border-bottom-color: rgb(159, 163, 163);")
         self.lineEdit.setObjectName("lineEdit")
 
-#################################Setting for typing input and print on enter###################################
+#################################Setting for typing input and print on enter################################### 
+       
 
-        # self.lineEdit.returnPressed.connect(self.txtstate)
-	       
+        self.lineEdit.returnPressed.connect(self.txtstate)
 ###############################################################################################################
      
         self.label = QtWidgets.QLabel(self.LoginFrame)
@@ -328,29 +337,44 @@ class UIToolTab(QWidget):
         self.CPSBTN.move(100, 350)
 
 
-class MainWindow(QMainWindow):
-    def __init__(self, parent=None):
-        super(MainWindow, self).__init__(parent)
-        self.setGeometry(50, 50, 400, 450)
-        self.setFixedSize(1280, 800)
-        self.setStyleSheet("background-color: rgb(255,252,241);");
+    def txtstate(self):
+        text_input = self.lineEdit.text()
+        text_file = open("ClinicanName.txt", "w")
+        text_file.write("%s" % text_input)
+        text_file.close()
 
-        self.startUIToolTab()
+        print(text_input)
 
-    def startUIToolTab(self):
-        self.ToolTab = UIToolTab(self)
-        self.setCentralWidget(self.ToolTab)
-        self.ToolTab.CPSBTN.clicked.connect(self.startUIWindow)
-        self.show()
-
-    def startUIWindow(self):
-        self.Window = UIWindow(self)
-        self.setCentralWidget(self.Window)
-        self.Window.ToolsBTN.clicked.connect(self.startUIToolTab)
-        self.show()
+    
+        
 
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    w = MainWindow()
-    sys.exit(app.exec_())
+
+# class MainWindow(QMainWindow):
+#     def __init__(self, parent=None):
+#         super(MainWindow, self).__init__(parent)
+#         self.setGeometry(50, 50, 400, 450)
+#         self.setFixedSize(1280, 800)
+#         self.setStyleSheet("background-color: rgb(255,252,241);");
+
+#         self.startUIToolTab()
+
+#     def startUIToolTab(self):
+#         self.ToolTab = UIToolTab(self)
+#         self.setCentralWidget(self.ToolTab)
+#         self.ToolTab.CPSBTN.clicked.connect(self.startUIWindow)
+#         self.show()
+
+#     def startUIWindow(self):
+#         self.Window = UIWindow(self)
+#         self.setCentralWidget(self.Window)
+#         self.Window.ToolsBTN.clicked.connect(self.startUIToolTab)
+#         self.show()
+
+
+# if __name__ == '__main__':
+#     app = QApplication(sys.argv)
+#     w = MainWindow()
+#     sys.exit(app.exec_())
+
+
